1e933abb2d7e640da7ea7ba55c08e6103c8c2721
GoopyCoder <143136712+GoopyCoder@users.noreply.github.com>
2023-09-22 21:26:49 -0400

    Add files via upload

---
+++ MIDI Project/Multi-Genre Instant Music Generator.py
@@ -0,0 +1,105 @@
+"""
+Isaac Powell
+The purpose of this code is to randomly generate a chord progression for a user-selected genre.
+"""
+
+import os
+from mido import MidiFile, MidiTrack, Message
+import random
+from CHORDS import chord_to_midi
+from Progressions import chord_progressions
+
+
+# Ask the user to select a genre
+genre_input = int(input("Select a genre (hip-hop (1), r&b (2), pop (3), jazz (4), OVO (5)): "))
+
+# Check if the entered genre is valid
+if genre_input > 0 and genre_input <= len(chord_progressions):
+    genres = list(chord_progressions.keys())
+    selected_genre = genres[genre_input - 1]
+
+    # Randomly select a chord progression from the chosen genre
+    selected_chord_progression = random.choice(chord_progressions[selected_genre])
+
+    # Print the selected chord progression
+    print(f"Random {selected_genre} chord progression: {selected_chord_progression}")
+else:
+    print("Invalid genre. Please select from hip-hop (1), r&b (2), pop (3), jazz (4), OVO (5).")
+
+chord_1 = selected_chord_progression[0]
+chord_2 = selected_chord_progression[1]
+chord_3 = selected_chord_progression[2]
+chord_4 = selected_chord_progression[3]
+
+midi_chord_1 = chord_to_midi.get(chord_1)
+midi_chord_2 = chord_to_midi.get(chord_2)
+midi_chord_3 = chord_to_midi.get(chord_3)
+midi_chord_4 = chord_to_midi.get(chord_4)
+
+# # Create a new MIDI file
+midi_file = MidiFile()
+
+# # Create new MIDI tracks
+track = MidiTrack()
+bass_track = MidiTrack()
+midi_file.tracks.append(track)
+midi_file.tracks.append(bass_track)
+
+midi_chords = [midi_chord_1, midi_chord_2, midi_chord_3, midi_chord_4]
+
+
+for midi_chord in midi_chords:
+    for note in midi_chord:
+        # print(note)
+        track.append(Message('note_on', note=note, velocity=64, time=0))
+    track.append(Message('note_off', note=midi_chord[0], velocity=64, time=1920))
+    if (len(midi_chord) ==5):
+        track.append(Message('note_off', note=midi_chord[1], velocity=64, time=0))
+        track.append(Message('note_off', note=midi_chord[2], velocity=64, time=0))
+        track.append(Message('note_off', note=midi_chord[3], velocity=64, time=0))
+        track.append(Message('note_off', note=midi_chord[4], velocity=64, time=0))
+    elif (len(midi_chord) ==4):
+        track.append(Message('note_off', note=midi_chord[1], velocity=64, time=0))
+        track.append(Message('note_off', note=midi_chord[2], velocity=64, time=0))
+        track.append(Message('note_off', note=midi_chord[3], velocity=64, time=0))
+    else:
+        track.append(Message('note_off', note=midi_chord[1], velocity=64, time=0))
+        track.append(Message('note_off', note=midi_chord[2], velocity=64, time=0))
+
+for midi_chord in midi_chords:
+    bass_track.append(Message('note_on', note=midi_chord[0], velocity=64, time=0))
+    bass_track.append(Message('note_off', note=midi_chord[0], velocity=0, time=1920))
+    if (len(midi_chord) ==5):
+        track.append(Message('note_off', note=midi_chord[1], velocity=0, time=0))
+        track.append(Message('note_off', note=midi_chord[2], velocity=0, time=0))
+        track.append(Message('note_off', note=midi_chord[3], velocity=0, time=0))
+        track.append(Message('note_off', note=midi_chord[4], velocity=0, time=0))
+    if (len(midi_chord) ==4):
+        bass_track.append(Message('note_off', note=midi_chord[1], velocity=0, time=0))
+        bass_track.append(Message('note_off', note=midi_chord[2], velocity=0, time=0))
+        bass_track.append(Message('note_off', note=midi_chord[3], velocity=0, time=0))
+    else:
+        bass_track.append(Message('note_off', note=midi_chord[1], velocity=0, time=0))
+        bass_track.append(Message('note_off', note=midi_chord[2], velocity=0, time=0))
+                          
+
+
+
+# Get the current working directory (where the script is located)
+current_directory = os.getcwd()
+
+# Specify the filename (you can customize this as needed)
+file_name = 'chord_midi_file.mid'
+
+# Specify the full file path, including the current directory, filename, and extension
+file_path = os.path.join(current_directory, file_name)
+
+# Create a new MIDI file
+midi_file = MidiFile()
+
+# ... (Your existing code to create MIDI tracks and add notes) ...
+
+# Save the MIDI file to the current directory
+midi_file.save(file_path)
+
+print(f"MIDI file saved to: {file_path}")
---
+++ MIDI Project/Progressions.py
@@ -0,0 +1,46 @@
+chord_progressions = {
+    "hip-hop": [
+        ["Am", "Em", "Dm", "G"],  # Similar to the style in "Lose Yourself" by Eminem
+        # ["F", "Am", "G", "E"],  # Similar to the style in "Sicko Mode" by Travis Scott (4th oct down)
+        ["Gm", "Eb", "Bb", "F"],  # Similar to the style in "Airplanes" by B.o.B
+        ["D", "Bm", "G", "A"],  # Similar to the style in "Stan" by Eminem
+        ["F", "C", "Dm", "Bb"],  # Similar to the style in "Hotline Bling" by Drake
+    ],
+    "r&b": [
+        ["Cmaj7", "Am7", "Dm7", "G7"],  # Similar to the style in "Un-break My Heart" by Toni Braxton
+        ["Bbmaj7", "Gm7", "Am7", "D7"],  # Similar to the style in "Adore You" by Harry Styles
+        ["Emaj7", "C#m7", "F#m7", "B7"],  # Similar to the style in "I Will Always Love You" by Whitney Houston
+        ["Amaj7", "F#m7", "Bm7", "Dmaj7"],  # Similar to the style in "No Scrubs" by TLC
+    ],
+    "pop": [
+        ["C", "G", "Am", "F"],  # Similar to the style in "Someone Like You" by Adele
+        ["G", "D", "Em", "C"],  # Similar to the style in "Shape of You" by Ed Sheeran
+        ["D", "A", "Bm", "G"],  # Similar to the style in "Shallow" by Lady Gaga & Bradley Cooper
+        ["F", "C", "Dm", "Bb"],  # Similar to the style in "Just the Way You Are" by Bruno Mars
+        ["Am", "F", "C", "G"],  # Similar to the style in "Love Yourself" by Justin Bieber
+        ["E", "B", "C#m", "A"],  # Similar to the style in "Teenage Dream" by Katy Perry
+        ["A", "E", "F#m", "D"],  # Similar to the style in "Viva la Vida" by Coldplay
+    ],
+    "jazz": [
+        ["Dm7", "G7", "Cmaj7", "F7"],  # Similar to the style in "Autumn Leaves" by Joseph Kosma
+        ["Am7", "D7", "Gmaj7", "Bm7"],  # Similar to the style in "Blue Monk" by Thelonious Monk
+        ["Fm7", "Bb7", "Ebmaj7", "Ab7"],  # Similar to the style in "Take Five" by Dave Brubeck
+    ],
+    "OVO": [
+        ["F#m7", "E", "Dmaj7", "C#m7"],  # Similar to the style in "One Dance"
+        ["Amaj7", "F#m7", "C#m7", "Bm7"],  # Similar to the style in "In My Feelings"
+        ["E", "Bm7", "C#m7", "A"],  # Similar to the style in "Hold On, We're Going Home"
+        ["Dmaj7", "C#m7", "F#m7", "E"],  # Similar to the style in "Too Good"
+        ["Am7", "G", "F", "C"],  # Similar to the style in "Take Care"
+        ["A", "E", "F#m7", "D"],  # Similar to the style in "Nice for What"
+        ["F#m", "D", "A", "E"],  # Similar to the style in "The Motto"
+        ["E", "C#m", "A", "B"],  # Similar to the style in "Controlla"
+        ["Am", "C", "G", "F"],  # Similar to the style in "Marvin's Room"
+        ["F", "C", "Dm", "Bb"],  # Similar to the style in "Energy"
+        ["Dm", "Am", "C", "G"],  # Similar to the style in "Jumpman" 
+        ["Bm", "G", "D", "A"],  # Similar to the style in "0 to 100 / The Catch Up"
+        ["F#m7", "D", "E", "A"],  # Similar to the style in "Trophies"
+        ["Bm7", "G", "D", "A"],  #Extra.
+
+    ]
+}
